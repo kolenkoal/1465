d1421c348c8942fc767ed71e1e03c66933d2b656
kolenkoal <kolenko.alexander0@gmail.com>
2023-07-13 21:21:03 +0300

    Initial commit

---
+++ Assets/SimpleAttach.cs
@@ -0,0 +1,48 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class SimpleAttach : MonoBehaviour
+{
+
+    private Interactable interactable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       interactable = GetComponent<Interactable>();
+
+    }
+
+    private void onHandHoverBegin(Hand hand) 
+    {
+        hand.ShowGrabHint();
+    }
+    private void onHandHoverEnd(Hand hand) 
+    {
+        hand.HideGrabHint();
+    }
+    private void onHandHoverUpdate(Hand hand) 
+    {
+        GrabTypes grabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
+
+        if (interactable.attachedToHand == null && grabType != GrabTypes.None)
+        {
+            hand.AttachObject(gameObject, grabType);
+            hand.HoverLock(interactable);
+            hand.HideGrabHint();
+        }
+
+        else if(isGrabEnding)
+        {
+            hand.DetachObject(gameObject);
+            hand.HoverUnlock(interactable);
+        }
+    }
+}
+
